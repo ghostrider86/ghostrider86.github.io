8bc574181511a531ce19a092d63b9c416b3dee5a
CIT 230 <elz20002@byui.edu>
2021-10-16 13:51:23 -0600

    lesson5 preston

---
+++ lesson5/preston.js
@@ -0,0 +1,31 @@
+
+// Function for toggling navigation menu.
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide")
+}
+// set variables for days and months
+var weekdays = new Array(
+    "Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday");
+var monthsInYear = new Array(
+    "January", "February", "March","April", "May", "June", "July", "August", "September","October", "November", "December"
+    );
+const year = new Date().getFullYear();
+const lastModified = new Date(document.lastModified);
+var day = lastModified.getDay();
+var day = weekdays[day];
+var month = lastModified.getMonth();
+var month = monthsInYear[month];
+
+var lastModifiedFormatted = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
+
+document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
+document.getElementById("copyrightYear").textContent = year;
+
+// pancake baner
+function createBanner(day) {
+    if (day == "Friday") {
+        document.querySelector("#banner").style.display = "block";
+    }
+}
+
+createBanner(day);

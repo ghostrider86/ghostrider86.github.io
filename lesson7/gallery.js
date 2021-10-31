e45e1c97da2aa1911f9fdaafeefd489b739fd035
CIT 230 <elz20002@byui.edu>
2021-10-30 23:09:10 -0600

    lesson7 edit

---
+++ lesson7/gallery.js
@@ -0,0 +1,55 @@
+
+// Function for toggling navigation menu.
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide")
+}
+// set variables for days and months
+var weekdays = new Array(
+    "Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday");
+var monthsInYear = new Array(
+    "January", "February", "March","April", "May", "June", "July", "August", "September","October", "November", "December"
+    );
+const year = new Date().getFullYear();
+const lastModified = new Date(document.lastModified);
+var day = lastModified.getDay();
+var day = weekdays[day];
+var month = lastModified.getMonth();
+var month = monthsInYear[month];
+
+var lastModifiedFormatted = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
+
+document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
+document.getElementById("copyrightYear").textContent = year;
+
+// images
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 0.5
+}
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}

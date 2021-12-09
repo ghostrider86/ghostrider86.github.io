bb15db2129186dce733b689c72db9542c0b7bba6
CIT 230 <elz20002@byui.edu>
2021-12-08 21:41:20 -0700

    fianl

--- final/js/other.js
+++ final/js/other.js
@@ -1,44 +1,22 @@
-//  Toggle menu
-
+// Function for toggling navigation menu.
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide")
 }
 
-// Last Updated 
-
-// Create formating for last updated information
-var weekday = new Array(
-    "Sunday", 
-    "Monday", 
-    "Tuesday", 
-    "Wednesday",
-    "Thursday", 
-    "Friday", 
-    "Saturday"
-    );
-var months = new Array(
-    "January", 
-    "February", 
-    "March",
-    "April", 
-    "May", 
-    "June", 
-    "July", 
-    "August", 
-    "September",
-    "October", 
-    "November", 
-    "December"
+// set variables for days and months
+var weekdays = new Array(
+    "Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday");
+var monthsInYear = new Array(
+    "January", "February", "March","April", "May", "June", "July", "August", "September","October", "November", "December"
     );
 const year = new Date().getFullYear();
 const lastModified = new Date(document.lastModified);
 var day = lastModified.getDay();
-var day = weekday[day];
+var day = weekdays[day];
 var month = lastModified.getMonth();
-var month = months[month];
+var month = monthsInYear[month];
 
 var lastModifiedFormatted = day + ', ' + lastModified.getDate() + ' ' + month + " " + lastModified.getFullYear();
 
-// Display copyright year and date last modified to footer of HTML document.
 document.getElementById("dateLastModified").textContent = lastModifiedFormatted;
 document.getElementById("copyrightYear").textContent = year;
